214469c26fda6761bed48ea1b33280b04f0f3055
Shobhit Sharma <shobhit.musiclover.sharma1@gmail.com>
2016-12-07 17:36:30 +0530

    Integrating Chat Module

--- ngApp/src/app.js
+++ ngApp/src/app.js
@@ -6,17 +6,12 @@
 var app = angular.module('retailerStock', ['ui.router', 'ngStorage', 'ngCookies', 'oitozero.ngSweetAlert', 'ui.bootstrap', /*'ui.calendar',
     */'ngAnimate', 'ngMessages'/*,'ngValidate','ngFileUpload', 'angular-confirm','textAngular' ,'chart.js' ,'angularMoment','uiSwitch'*/])
         .constant({
-          'SERVER_URL': function ($location) {
-            if (location.hostname === "localhost" || $location.hostname === "127.0.0.1")
-              return 'http://localhost:3000/';
-            else return 'http://https://retailerstock.herokuapp.com/';
-          }
+          'SERVER_URL': (location.hostname === "localhost" || location.hostname === "127.0.0.1") ? 'http://localhost:3000/' : 'https://retailerstock.herokuapp.com/'
 
 });
 
 app.run(['$rootScope', 'SERVER_URL', '$templateCache',
   function($rootScope, SERVER_URL, $templateCache) {
-
 
     $rootScope.TEMPLATE_URL = SERVER_URL+'views/';
     $rootScope.IMG_URL      = SERVER_URL+'images/';
