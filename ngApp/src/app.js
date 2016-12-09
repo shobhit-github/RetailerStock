87d6b7d2695eb158297852d49c6284c53e589ca2
Shobhit Sharma <shobhit.musiclover.sharma1@gmail.com>
2016-12-09 14:41:36 +0530

    Additional Changes

--- ngApp/src/app.js
+++ ngApp/src/app.js
@@ -10,17 +10,13 @@ var app = angular.module('retailerStock', ['ui.router', 'ngStorage', 'ngCookies'
 
 });
 
-app.run(['$rootScope', 'SERVER_URL', '$templateCache', '$qProvider',
-  function($rootScope, SERVER_URL, $templateCache, $qProvider) {
+app.run(['$rootScope', 'SERVER_URL', '$templateCache',
+  function($rootScope, SERVER_URL, $templateCache) {
 
     $rootScope.TEMPLATE_URL = SERVER_URL+'views/';
     $rootScope.IMG_URL      = SERVER_URL+'images/';
     $rootScope.FILE_URL     = SERVER_URL+'files/';
 
-    var $injector2 = window.angular.injector(['ng']);
-    $injector2.invoke(['$qProvider', function($qProvider) {
-      $qProvider.errorOnUnhandledRejections(false);
-    }]);
 
 
     var clearTemplateCache = function () {
