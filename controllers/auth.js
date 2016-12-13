d32ec4d05e8bcd9b18e6a4c4c03cdf86a396ccbc
Shobhit Sharma <shobhit.musiclover.sharma1@gmail.com>
2016-12-13 23:33:39 +0530

    Additional Changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,7 +35,7 @@ var methods = new Object();
  */
 methods.checkAuth = function(req, res) {
 
-  /*client.sendMessage( { to:'+917837098699', from:'+14698282295', body:'NodeJS Test SMS Gateway by Shobhit Sharma' }, function( err, data ) {
+  /*client.sendMessage( { to:'+919041155693', from:'+14698282295', body:'NodeJS Test SMS Gateway by Shobhit Sharma' }, function( err, data ) {
 
     if(err) console.log(err);
     else console.log(data);
