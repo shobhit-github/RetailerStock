1d76044130af17926d81915ac162c8496b02a31a
Shobhit Sharma <shobhit.musiclover.sharma1@gmail.com>
2016-12-13 18:02:53 +0530

    SMS Gateway Integration

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,7 +34,7 @@ var methods = new Object();
  */
 methods.checkAuth = function(req, res) {
 
-  client.sendMessage( { to:'+917837098699', from:'+14698282295', body:'NodeJS Test SMS Gateway by Shobhit Sharma' }, function( err, data ) {
+  //client.sendMessage( { to:'+917837098699', from:'+14698282295', body:'NodeJS Test SMS Gateway by Shobhit Sharma' }, function( err, data ) {
 
     if(err) console.log(err);
     else console.log(data);
