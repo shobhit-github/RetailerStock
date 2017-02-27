77accf93eff5ea51de4cbfaba510288e40f3f34a
Shobhit Sharma <shobhit.musiclover.sharma1@gmail.com>
2017-02-27 19:10:55 +0530

    Added -> notification service added

---
+++ ngApp/src/services/notificationService.js
@@ -0,0 +1,42 @@
+
+"use strict";
+/**
+ * Storing multiple constant values inside of an object
+ * Keep in mind the values in the object mean they can be modified
+ * Which makes no sense for a constant, use wisely if you do this
+ */
+
+app.service('$notify', [ '$timeout', '$window', '$msg',
+	function ($timeout, $window, $msg) {
+
+
+    const styling = 'bootstrap3';
+
+    this.default = function(title, message) {
+
+        return new PNotify({ title: title || "Notification", text: message, type: 'info', styling: styling, addclass: 'dark', hide: false });
+    };
+
+	this.success = function (title, message) {
+
+        new PNotify({ title: title || "Success", text: message, type: 'success', styling: styling, hide: false });
+    };
+
+    this.error = function (title, message) {
+
+        new PNotify({ title: title || "Oh Shit !!", text: message, type: 'error', styling: styling, hide: false });
+    };
+
+    this.warning = function (title, message) {
+
+        new PNotify({ title: title || "Warning !", text: message, styling: styling, hide: false });
+    };
+
+    this.info = function (title, message) {
+
+        new PNotify({ title: title || "Information", text: message, type: 'info', styling: styling, hide: false });
+    }
+
+
+}]);
+
